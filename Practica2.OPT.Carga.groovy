70887fe03bdbd13bff2d52636f1755aadb640c6e
Pablo Pazos <pablo.pazos@cabolabs.com>
2016-08-17 01:25:20 -0300

    agreego ejemplos para curso de implementacion

---
+++ Practica2.OPT.Carga.groovy
@@ -0,0 +1,46 @@
+import com.cabolabs.openehr.opt.parser.*
+import com.cabolabs.openehr.opt.manager.*
+
+def path = "C:\\Documents and Settings\\pab\\My Documents\\GitHub\\openEHR-OPT\\resources\\opts"
+def path_to_opt = path + "\\Encuentro.opt"
+
+// Carga un OPT usando el parser directamente
+def parser = new OperationalTemplateParser()
+def optFile = new File( path_to_opt )
+def text = optFile.getText()
+      
+def opt = parser.parse( text )
+
+// Nodos y rutas del OPT
+def nodes = opt.nodes.sort{ it.key }
+nodes.each { opt_path, node ->
+   println opt_path.padRight(190, '.') +" ref: "+ ((node.archetypeId ?: node.nodeId) ?: node.rmTypeName)
+}
+
+// Nodos y rutas de arquetipos
+nodes.each { opt_path, node ->
+   node.nodes.each { arch_path, subnode ->
+      println arch_path.padRight(80, '.') +" ref: "+ ((subnode.archetypeId ?: subnode.nodeId) ?: subnode.rmTypeName)
+   }
+}
+
+
+// Carga todos los OPTs de un directorio usando el OptManager
+def man = OptManager.getInstance(path)
+man.loadAll()
+
+// Todos los OPTs cargados
+man.getLoadedOpts().keySet().each { templateId ->
+   println templateId
+}
+
+// Todos los arquetipos referenciados dsde todos los OPTs cargados
+man.getAllReferencedArchetypes().keySet().each { archId ->
+   println archId
+}
+
+return
+
+
+
+
---
+++ Practica2.OPT.Recorrida.groovy
@@ -0,0 +1,38 @@
+import com.cabolabs.openehr.opt.manager.*
+import com.cabolabs.openehr.opt.model.*
+
+def path = "C:\\Documents and Settings\\pab\\My Documents\\GitHub\\openEHR-OPT\\resources\\opts"
+
+// Carga todos los OPTs de un directorio usando el OptManager
+def man = OptManager.getInstance(path)
+man.loadAll()
+
+// Obtener un OPT por su templateId
+opt = man.getOpt('Encuentro')
+
+// Recorrida
+traverse(opt.definition, 0)
+
+def traverse(ObjectNode o, int pad)
+{
+   println " ".multiply(pad) + o.rmTypeName.padRight(35-pad, '.') + (o.archetypeId ?: o.path)
+   
+   pad++
+   o.attributes.each{
+      traverse(it, pad)
+   }
+}
+
+def traverse(AttributeNode a, int pad)
+{
+   println " ".multiply(pad) + a.rmAttributeName
+   
+   pad++
+   a.children.each{
+      traverse(it, pad)
+   }
+}
+
+man.unloadAll()
+
+return
