656765df13fa490978059b8e239bc3638f68528e
Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
2018-03-17 14:28:51 -0300

    compile, package and run files added for linux

---
+++ jargen.sh
@@ -0,0 +1,2 @@
+@REM jar -cfv openEHR_OPT.jar -C bin .
+jar -cvmf MANIFEST.txt openEHR_OPT.jar -C bin . -C .. xsd/*
---
+++ opt.sh
@@ -0,0 +1,1 @@
+java -cp "./lib/*:$GROOVY_HOME/embeddable/groovy-all-2.4.11.jar:openEHR_OPT.jar" com.cabolabs.openehr.opt.Main $1 $2 $3 $4 $5 $6
