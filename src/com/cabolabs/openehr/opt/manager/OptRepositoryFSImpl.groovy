75bb4556b1775da571c4dbeea4d7be12441129a8
Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
2020-05-26 15:07:10 -0300

    fixed issue with template id normalization

--- src/com/cabolabs/openehr/opt/manager/OptRepositoryFSImpl.groovy
+++ src/com/cabolabs/openehr/opt/manager/OptRepositoryFSImpl.groovy
@@ -1,10 +1,12 @@
 package com.cabolabs.openehr.opt.manager
 
 import net.pempek.unicode.UnicodeBOMInputStream
+import org.apache.log4j.Logger
 
 class OptRepositoryFSImpl implements OptRepository {
 
    private static String PS = File.separator
+   private Logger log = Logger.getLogger(getClass())
 
    // This is the path to the root repo location in the file system
    private String repoLocation
@@ -19,12 +21,30 @@ class OptRepositoryFSImpl implements OptRepository {
       this.repoLocation = repoLocation
    }
 
+   boolean storeOptContents(String fileLocation, String fileContents)
+   {
+      // creates parent subfolders if dont exist
+      def containerFolder = new File(new File(fileLocation).getParent())
+      containerFolder.mkdirs()
+
+      File fileDest = new File(fileLocation)
+
+      if (fileDest.exists())
+      {
+         log.warn "File "+ fileLocation +" already exists, overwriting"
+      }
+
+      fileDest.write(this.removeBOM(fileContents.getBytes())) // overwrites if file exists
+
+      return true // TODO check errors
+   }
+
    /**
-   * location is a key used to reference one OPT, could be an absolute file path,
-   * or an S3 object key. The namespace is already included in the location as a
-   * prefix.
-   * Returns null if no OPT was found.
-   */
+    * location is a key used to reference one OPT, could be an absolute file path,
+    * or an S3 object key. The namespace is already included in the location as a
+    * prefix.
+    * Returns null if no OPT was found.
+    */
    String getOptContents(String location)
    {
       def optFile = new File(location)
@@ -36,26 +56,31 @@ class OptRepositoryFSImpl implements OptRepository {
    }
 
    /**
-   * Does a search in the namespace and finds the first OPT that matches the templateId.
-   * Returns null if no OPT was found.
-   */
+    * Does a search in the namespace and finds the first OPT that matches the templateId.
+    * Returns null if no OPT was found.
+    */
    String getOptContentsByTemplateId(String templateId, String namespace)
    {
       // The file path should be:
       // namespace + / + normalized(templateId) + .opt
       // normalized(templateId) adds undescores for spaces and is all lower case
 
-      def normalizedTemplateId = getNormalizedTemplateId(templateId)
+      if (!isNormalizedTemplateId(templateId))
+      {
+         templateId = normalizeTemplateId(templateId)
+      }
+
       def location = addTrailingSeparator(this.repoLocation) +
                      addTrailingSeparator(namespace) +
-                     normalizedTemplateId + '.opt'
+                     templateId + '.opt'
+
       return getOptContents(location)
    }
 
    /**
-   * Returns the contents of all the OPTs under the given namespace.
-   * Returns an empty list if there are no OPTs.
-   */
+    * Returns the contents of all the OPTs under the given namespace.
+    * Returns an empty list if there are no OPTs.
+    */
    List<String> getAllOptContents(String namespace)
    {
       def result = []
@@ -85,15 +110,22 @@ class OptRepositoryFSImpl implements OptRepository {
       return result
    }
 
-   private String getNormalizedTemplateId(String templateId)
+   String normalizeTemplateId(String templateId, String language)
    {
       // https://gist.github.com/ppazos/12f3efc4eb178e43ff73a0c989a2e1d7
       String normalized = java.text.Normalizer.normalize(templateId, java.text.Normalizer.Form.NFD).replaceAll(/\p{InCombiningDiacriticalMarks}+/, '')
-      String camel = normalized.replaceAll( / ([A-Z])/, /$1/ ).replaceAll( /([A-Z])/, /_$1/ ).replaceAll(/\s/, '_').toLowerCase().replaceAll( /^_/, '' )
-      return camel
+      String snake      = normalized.replaceAll( / ([A-Z])/, /$1/ ).replaceAll( /([A-Z])/, /_$1/ ).replaceAll(/\s/, '_').toLowerCase().replaceAll( /^_/, '' )
+      String removeDots = snake.replaceAll(/\./, '_')
+      String plusLAndV  = removeDots +'.'+ language +'.v1'
+      return plusLAndV
    }
 
-   private String addTrailingSeparator(String path)
+   boolean isNormalizedTemplateId(String templateId)
+   {
+      (templateId ==~ /([a-z]+(_[a-z0-9]+)*)\.([a-z]{2})\.v([0-9]+[0-9]*(\.[0-9]+[0-9]*(\.[0-9]+[0-9]*)?)?)/)
+   }
+
+   String addTrailingSeparator(String path)
    {
       if(path.charAt(path.length()-1) != File.separatorChar)
       {
@@ -103,12 +135,24 @@ class OptRepositoryFSImpl implements OptRepository {
       return path
    }
 
-   private String removeBOM(byte[] bytes)
+   String removeBOM(byte[] bytes)
    {
       def inputStream = new ByteArrayInputStream(bytes)
       def bomInputStream = new UnicodeBOMInputStream(inputStream)
       bomInputStream.skipBOM() // NOP if no BOM is detected
       def br = new BufferedReader(new InputStreamReader(bomInputStream))
       return br.text // http://docs.groovy-lang.org/latest/html/groovy-jdk/java/io/BufferedReader.html#getText()
+   }
+
+   String newOptFileLocation(String templateId, String language, String namespace)
+   {
+      if (!isNormalizedTemplateId(templateId))
+      {
+         templateId = normalizeTemplateId(templateId, language)
+      }
+
+      addTrailingSeparator(repoLocation) +
+      addTrailingSeparator(namespace) +
+      templateId + '.opt'
    }
 }
