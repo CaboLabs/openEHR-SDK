a62c0eb19dee346ad818a09e542f48707b851ba9
ppazos <pablo.swp@gmail.com>
2021-04-24 15:42:48 -0300

    fixed cdvqty validation

--- src/main/groovy/com/cabolabs/openehr/opt/model/domain/CDvQuantity.groovy
+++ src/main/groovy/com/cabolabs/openehr/opt/model/domain/CDvQuantity.groovy
@@ -20,11 +20,15 @@ class CDvQuantity extends ObjectNode {
 
    ValidationResult isValid(String units, float magnitude)
    {
-      def item = list.find { it.units == units }
+      // if there are no constraints, the validation is always true
+      if (list.size() > 0)
+      {
+         def item = list.find { it.units == units }
 
-      if (!item) return new ValidationResult(isValid: false, message:'CDvQuantity.validation.error.noMatchingUnits')
+         if (!item) return new ValidationResult(isValid: false, message:'CDvQuantity.validation.error.noMatchingUnits')
 
-      if (!item.magnitude.has(magnitude)) return new ValidationResult(isValid: false, message:'CDvQuantity.validation.error.magnitudeOutOfRange')
+         if (!item.magnitude.has(magnitude)) return new ValidationResult(isValid: false, message:'CDvQuantity.validation.error.magnitudeOutOfRange')
+      }
 
       return new ValidationResult(isValid: true)
    }
