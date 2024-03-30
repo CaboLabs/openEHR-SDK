63a4940af8ce2946fb54424e3b3fed59c3f5bf0e
Pablo Pazos <pablo.pazos@cabolabs.com>
2024-03-30 13:46:21 -0300

    prepare next release

--- opt.sh
+++ opt.sh
@@ -1,1 +1,1 @@
-java -cp "./lib/*:$GROOVY_HOME/lib/*:build/libs/opt-1.9.27.jar" com.cabolabs.openehr.opt.Main $1 $2 $3 $4 $5 $6 $7
+java -cp "./lib/*:$GROOVY_HOME/lib/*:build/libs/opt-1.9.28.jar" com.cabolabs.openehr.opt.Main $1 $2 $3 $4 $5 $6 $7
