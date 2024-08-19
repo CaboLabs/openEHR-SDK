ec2b7792f71d7624de415477a5688c6f847623dd
Pablo Pazos <pablo.pazos@cabolabs.com>
2024-08-19 04:08:40 -0300

    added util constructors on some model classes #193

--- src/main/groovy/com/cabolabs/openehr/rm_1_0_2/data_types/quantity/date_time/DvDate.groovy
+++ src/main/groovy/com/cabolabs/openehr/rm_1_0_2/data_types/quantity/date_time/DvDate.groovy
@@ -1,8 +1,22 @@
 package com.cabolabs.openehr.rm_1_0_2.data_types.quantity.date_time
 
+import java.text.SimpleDateFormat
+
 class DvDate extends DvTemporal {
 
    String value
+
+   // Prevents groovy.lang.GroovyRuntimeException: Could not find named-arg compatible constructor with the second constructor
+   DvDate()
+   {
+
+   }
+
+   DvDate(Date date)
+   {
+      SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); // ISO date format
+      this.value = formatter.format(date)
+   }
 
    @Override
    public Number getMagnitude() {
