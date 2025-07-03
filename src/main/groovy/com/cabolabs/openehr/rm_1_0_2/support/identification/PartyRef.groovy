1b91120c4b1abae7826b688a2476fb83f1766c6b
Pablo Pazos <pablo.pazos@cabolabs.com>
2025-07-03 16:41:44 -0300

    working on improvements for the constructors

--- src/main/groovy/com/cabolabs/openehr/rm_1_0_2/support/identification/PartyRef.groovy
+++ src/main/groovy/com/cabolabs/openehr/rm_1_0_2/support/identification/PartyRef.groovy
@@ -1,5 +1,16 @@
 package com.cabolabs.openehr.rm_1_0_2.support.identification
 
+import
+
 class PartyRef extends ObjectRef {
 
+   PartyRef()
+   {
+      super()
+   }
+
+   PartyRef(HierObjectId id, String type, String namespace)
+   {
+      super(id, type, namespace)
+   }
 }
