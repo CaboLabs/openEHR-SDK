8929a974f30e3bb2a0eeeb23c57d4ed3439d7901
Pablo Pazos <pablo.pazos@cabolabs.com>
2026-05-02 13:53:31 -0300

    fixed log format

--- src/main/groovy/com/cabolabs/openehr/opt/model/ObjectNode.groovy
+++ src/main/groovy/com/cabolabs/openehr/opt/model/ObjectNode.groovy
@@ -85,12 +85,12 @@ class ObjectNode extends Constraint {
 
       if (!codedTerm)
       {
-         log.info( "codedTerm not found "+ this.archetypeId +" "+ code )
+         log.warn( "codedTerm not found "+ this.archetypeId +" "+ code )
          return
       }
       else
       {
-         log.info( "codedTerm found "+ codedTerm.code +" "+ codedTerm.term.text )
+         //log.info( "codedTerm found "+ codedTerm.code +" "+ codedTerm.term.text )
       }
 
       return codedTerm.term.description
